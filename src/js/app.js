9fbeaf9e05d8e96bd80d8b0bd91d19cccdf2141d
Konstantin Muzyka <kosatkinms@mail.ru>
2022-04-20 00:19:51 +0300

    create class appSection

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,7 @@
+import AppSection from './components/AppSection';
 
+window.onload = () => {
+  const todo = new AppSection('todo');
+  const inProgress = new AppSection('in-progress');
+  const done = new AppSection('done');
+};
---
+++ src/js/components/AppSection.js
@@ -0,0 +1,65 @@
+export default class AppSection {
+  constructor(sectionId) {
+    this.section = document.getElementById(sectionId);
+    this.cardList = this.section.querySelector('.app__card-list');
+    this.openFormBtn = this.section.querySelector('.app__add-card-btn');
+
+    this.addCardForm = this.section.querySelector('.app__form');
+    this.addCardTextarea = this.addCardForm.querySelector('.form__textarea');
+    this.closeFormBtn = this.addCardForm.querySelector('.close-btn');
+
+    // LISTENERS
+
+    this.section.addEventListener('click', (evt) => {
+      if (evt.target == this.openFormBtn || evt.target == this.closeFormBtn) {
+        this.toggleForm();
+        this.addCardForm.reset();
+        return;
+      }
+    });
+
+    this.addCardForm.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+
+      if (this.addCardTextarea.value !== '') {
+        this.addCard(this.addCardTextarea.value);
+        evt.currentTarget.reset();
+        this.toggleForm();
+        return;
+      } else {
+        return alert('Empty');
+      }
+    });
+  }
+
+  renderCard(value) {
+    const card = document.createElement('li');
+    card.classList.add('app__card');
+    card.textContent = `${value}`;
+
+    const closeBtn = document.createElement('a');
+    closeBtn.href = '#';
+    closeBtn.classList.add('close-btn', 'card__close-btn');
+    card.appendChild(closeBtn);
+
+    card.addEventListener('mouseover', () => {
+      closeBtn.style.opacity = '1';
+    });
+    card.addEventListener('mouseout', () => {
+      closeBtn.style.opacity = '0';
+    });
+    closeBtn.addEventListener('click', () => {
+      card.remove();
+    });
+    return card;
+  }
+
+  addCard(value) {
+    this.cardList.appendChild(this.renderCard(value));
+  }
+
+  toggleForm() {
+    this.openFormBtn.classList.toggle('hidden');
+    this.addCardForm.classList.toggle('hidden');
+  }
+}
